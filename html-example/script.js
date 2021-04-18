be97694e2abbe1771e9100656d38be58c623ad52
Okhan97 <iapenafiel@uc.cl>
2021-04-18 19:51:58 -0400

    todo

--- html-example/script.js
+++ html-example/script.js
@@ -1,6 +1,5 @@
-const {fromEvent} = rxjs;
-const { map, tap, switchMap, delay } = rxjs.operators;
-const of = rxjs.of;
+const {fromEvent, of, from, timer} = rxjs;
+const { map, tap, switchMap, delay, takeWhile, throttleTime} = rxjs.operators;
 const interval = rxjs.interval;
 
 import {player} from './con.js';
@@ -24,13 +23,18 @@ const static_div = document.createElement('div');
 static_div.style.height = '20px';
 static_div.style.height = '20px';
 
+let end_game = false;
+let p1_dead = false;
+let p2_dead = false;
+
 const player1 = document.createElement('div')
 player1.style.display = 'inline-block';
 player1.style.marginTop = '52px'; // 32 tope superior
 player1.style.height = '20px';
 player1.style.width = '20px';
 player1.style['background-color'] ='blue';
-player1.style.borderRadius = '40%'
+//player1.style['background-image'] = "url('smile.gif')";
+player1.style.borderRadius = '0%'
 
 const static_div_2 = document.createElement('div');
 
@@ -48,18 +52,19 @@ static_div.appendChild(player1)
 
 fromEvent(document, 'click')
   .pipe(
+  	takeWhile(() => !end_game),
+    throttleTime(600),
     switchMap(() => player1.style.marginTop = '42px'),
     delay(100),
     switchMap(() => player1.style.marginTop = '37px'),
     delay(100),
     switchMap(() => player1.style.marginTop = '32px'),
-    delay(500),
+    delay(300),
     switchMap(() => player1.style.marginTop = '42px'),
     delay(100),
     switchMap(() => player1.style.marginTop = '52px'),
-
   )
-  .subscribe(console.log);
+  .subscribe();
 
   document.getElementById('game').appendChild(static_div_2);
   static_div_2.appendChild(player2)
@@ -67,15 +72,86 @@ fromEvent(document, 'click')
 
 fromEvent(document, 'keyup')
   .pipe(
+    takeWhile(() => !end_game),
+    throttleTime(600),
     switchMap(() => player2.style.marginTop = '92px'),
     delay(100),
     switchMap(() => player2.style.marginTop = '87px'),
     delay(100),
     switchMap(() => player2.style.marginTop = '82px'),
-    delay(500),
+    delay(300),
     switchMap(() => player2.style.marginTop = '92px'),
     delay(100),
     switchMap(() => player2.style.marginTop = '102px'),
-
   )
-  .subscribe(console.log);
+  .subscribe();
+
+
+const static_div_3 = document.createElement('div');
+const static_div_4 = document.createElement('div');
+
+static_div_3.style.height = '20px';
+static_div_3.style.width = '200px';
+
+static_div_4.style.height = '20px';
+static_div_4.style.width = '200px';
+
+
+document.getElementById('game').appendChild(static_div_3);
+document.getElementById('game').appendChild(static_div_4);
+
+let marginle1 = 350;
+let marginle2 = 350;
+
+const object1 = document.createElement('div')
+object1.style.display = 'inline-block';
+object1.style.marginTop = '37px'; // 32 tope superior
+object1.style.marginLeft = marginle1 + 'px';
+object1.style.height = '15px';
+object1.style.width = '20px';
+object1.style['background-color'] ='green';
+
+const object2 = document.createElement('div')
+object2.style.display = 'inline-block';
+object2.style.marginTop = '87px';
+object2.style.marginLeft = marginle2 + 'px';
+object2.style.height = '15px';
+object2.style.width = '20px';
+object2.style['background-color'] ='green';
+
+static_div_3.appendChild(object1);
+static_div_4.appendChild(object2);
+
+const numbers = interval(20);
+
+numbers.subscribe(() => {
+  if (!end_game){
+    marginle1 -= 5;
+    marginle2 -= 5;
+  }
+
+  if (marginle1 <= 20 && player1.style.marginTop === '52px'){
+    p1_dead = true;
+  }
+  if (marginle2 <= 20 && player2.style.marginTop === '102px'){
+    p2_dead = true;
+  }
+  end_game = p1_dead || p2_dead;
+
+	if (marginle1 <=-40) {
+		marginle1 = 350 + Math.floor(Math.random() * 350);
+	}
+	if (marginle2 <=-40) {
+		marginle2 = 350 + Math.floor(Math.random() * 350);
+	}
+	object1.style.marginLeft = marginle1 + 'px';
+  object1.style['opacity'] = marginle1 >= 350 ? 0 : 1;
+	object2.style.marginLeft = marginle2 + 'px';
+  object2.style['opacity'] = marginle2 >= 350 ? 0 : 1;
+})
+
+
+
+
+
+
